6c885b1621661fab4c72d85751dd8cb9075ff203
Kershia1 <insatiable.meerkat@gmail.com>
2024-03-11 06:08:51 +0000

    Fix logout route

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
     # these routes are for showing users a login form, logging them in, and logging them out.
     get '/login' => 'sessions#new'
     post '/login' => 'sessions#create'
-    get '/logout' => 'sessions#destroy'
+    delete '/logout' => 'sessions#destroy'
+    # get '/logout' => 'sessions#destroy'
 
   #user routes
      # These routes will be for signup. The first renders a form in the browse, the second will 
