fa2556fa3f0b2c472dba6f3b3f7fbc510d97f687
Kershia1 <insatiable.meerkat@gmail.com>
2024-03-11 04:39:29 +0000

    ADD session logic to controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password]) # authenticate is a method provided by has_secure_password
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      redirect_to '/login'
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to '/login'
   end
 
 end
