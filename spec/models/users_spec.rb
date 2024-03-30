6ea99219cc6aa4792d1d72cdd6725f7627f3c23d
Kershia1 <insatiable.meerkat@gmail.com>
2024-03-30 23:52:47 +0000

    odified

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -3,3 +3,14 @@ require 'rails_helper'
 RSpec.describe Users, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
+# define validations specs for the User model
+
+# password and password_confirmation should match
+
+# email should be unique (not case sensitive)
+
+#  Define a new class method authenticate_with_credentials on the User model
+
+# Edge cases to consider:
+# email has white spaces around it
+# email is in different cases
