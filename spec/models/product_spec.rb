30d2f4fde9f52e5f63bdc8024f22b917b5a72389
Kershia1 <insatiable.meerkat@gmail.com>
2024-03-30 23:43:34 +0000

    ADD all tests pass

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+
+    # validate :name, presence: true
+    it 'validates name presence' do
+      @product = Product.new(name: nil, price: 100, quantity: 10, category: @catergory)
+      @product.save
+      expect(@product.errors.full_messages).to include("Name can't be blank")
+    end
+
+    # validate :price, presence: true
+    it 'validates price presence' do
+      @product = Product.new(name: 'Tea Tree', quantity: 10, category: @catergory)
+      @product.save
+      expect(@product.errors.full_messages).to include("Price can't be blank")
+    end
+
+    # validate :quantity, presence: true
+    it 'validates quantity presence' do
+      @product = Product.new(name: 'Tea Tree', price: 100, quantity: nil, category: @catergory)
+      @product.save
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
+    end
+
+    # validate :category, presence: true
+    it 'validates category presence' do
+      @product = Product.new(name: 'Tea Tree', price: 100, quantity: 10, category: nil)
+      @product.save
+      expect(@product.errors.full_messages).to include("Category can't be blank")
+    end
+  end
 end
