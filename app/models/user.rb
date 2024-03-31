299e340be2fdba8b23a4ac8ab27da622791e85c8
Kershia1 <insatiable.meerkat@gmail.com>
2024-03-31 00:48:48 +0000

    ADD user login authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
   validates :email, uniqueness: { case_sensitive: false }
   has_secure_password
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email.strip.downcase)
+     if user && user.authenticate(password)
+      user
+     else
+      nil
+     end
+  end
 end
--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -17,14 +17,6 @@ RSpec.describe User, type: :model do
     end
 
   # email should be unique (not case sensitive)
-    # it 'should not save the user when email is not unique' do
-    #   @user_1 = User.new(name: 'Jane', email: 'j.doe.smith@yahoo.com', password: 'passwordZ0', password_confirmation: 'passwordZ0')
-    #   @user_1.save
-    #   @user_2 = User.new(name: 'Joey', email: 'j.doe.smith@yahoo.com', password: 'password0', password_confirmation: 'password0')
-    #   @user_2.save
-    #   expect(@user_2.errors.full_messages).to include("Email has already been taken")
-    # end
-
     it 'should not save the user when email is not unique' do
       @user_1 = User.new(name: 'Jane', email: 'j.doe.smith@yahoo.com'.downcase, password: 'passwordZ0', password_confirmation: 'passwordZ0')
       @user_1.save
@@ -32,11 +24,21 @@ RSpec.describe User, type: :model do
       @user_2.save
       expect(@user_2.errors.full_messages).to include("Email has already been taken")
     end
+
     #  Define a new class method authenticate_with_credentials on the User model
+    #  This method will take the email and password from the params and return the user if the user exists and the password is correct
+    #  Otherwise, it will return nil
+
+    describe '.authenticate_with_credentials' do
+      it 'should log-in user only when credentials are valid' do
+      @user = User.new(name: 'Jane', email: 'j.doe.smith@yahoo.com', password: 'passwordZ0', password_confirmation: 'passwordZ0')
+      @user.save
+      @user_logged_in = User.authenticate_with_credentials('j.doe.smith@yahoo.com', 'passwordZ0')
+      expect(@user_logged_in).to_not be_nil
+     end
 
 # Edge cases to consider:
 # email has white spaces around it
-# email is in different cases
-
+    end
   end
 end
