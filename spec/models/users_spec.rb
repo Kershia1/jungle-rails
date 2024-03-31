b59ad9f6d03f586b61c102280fc49f446a0eb1c8
Kershia1 <insatiable.meerkat@gmail.com>
2024-03-31 00:25:32 +0000

    ADD Password confirmation test

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -3,19 +3,20 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'Validation' do
 
+  # define validations specs for the User model
     it 'should save a new user only when all required fields are filled' do
       @user = User.new(name: 'Jane', email: 'j.doe.smith@yahoo.com', password: 'passwordZ0', password_confirmation: 'passwordZ0')
       expect(@user).to be_valid
     end
+
+  # password and password_confirmation should match
+    it 'should not save a new user when password and password_confirmation do not match' do
+      @user = User.new(name: 'Jane', email: 'j.doe.smith@yahoo.com', password: 'passwordZ0', password_confirmation: 'password')
+      @user.save
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
   end
 end
-
-
-
-
-# define validations specs for the User model
-
-# password and password_confirmation should match
 
 # email should be unique (not case sensitive)
 
